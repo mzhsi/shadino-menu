6184e91640295a2fd06f06cfb5d08d30525a6f2b
mzhsi <mohammad.64859@gmail.com>
2026-09-24 18:01:31 +0330

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+// Supabase configuration
+
+window.SUPABASE_URL = "https://dxhbqegryhcyfkiqtjov.supabase.co";
+
+window.SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_2GKiUMB7M6lVntVsNj7ZbQ_8JS1bB5i";
