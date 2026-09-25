464f706697ca3d23bbb2c8a764b47b59c0c49e80
mzhsi <mohammad.64859@gmail.com>
2026-09-25 17:58:50 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 // config.js
 
-const SUPABASE_URL = "https://zherltvxobfntipbfzxq.supabase.co";
+window.SUPABASE_URL = "https://zherltvxobfntipbfzxq.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY =
+window.SUPABASE_PUBLISHABLE_KEY =
   "sb_publishable_ZyhQXvFM882k5JbQolgKTw_g5JvsMQs";
