46bff7954c003428734203d1ff2f066ad0d6b3a8
mzhsi <mohammad.64859@gmail.com>
2026-09-25 17:54:50 +0330

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
-// Supabase configuration
+// config.js
 
-window.SUPABASE_URL = "https://dxhbqegryhcyfkiqtjov.supabase.co";
+const SUPABASE_URL = "https://zherltvxobfntipbfzxq.supabase.co";
 
-window.SUPABASE_PUBLISHABLE_KEY =
-  "sb_publishable_2GKiUMB7M6lVntVsNj7ZbQ_8JS1bB5i";
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_ZyhQXvFM882k5JbQolgKTw_g5JvsMQs";
