476e36880d0afaaa0836de57f7b24ce83eb19706
mzhsi <mohammad.64859@gmail.com>
2026-09-25 18:03:03 +0330

    Update config.js

--- kama/config.js
+++ kama/config.js
@@ -1,11 +1,4 @@
-// config.js
+window.SUPABASE_URL = "https://zherltvxobfntipbfzxq.supabase.co";
 
-const SUPABASE_URL = "https://zherltvxobfntipbfzxq.supabase.co";
-
-const SUPABASE_ANON_KEY =
+window.SUPABASE_PUBLISHABLE_KEY =
   "sb_publishable_ZyhQXvFM882k5JbQolgKTw_g5JvsMQs";
-
-const supabaseClient = window.supabase.createClient(
-  SUPABASE_URL,
-  SUPABASE_ANON_KEY
-);
